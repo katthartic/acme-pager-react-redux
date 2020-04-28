eb7d84448b567f38f8b4e430e867a90d0e98e353
katthartic <katt.baum@gmail.com>
2020-04-27 20:41:34 -0400

    Make non-stateful

--- src/App.js
+++ src/App.js
@@ -2,42 +2,17 @@ import React, { Component } from 'react'
 import { HashRouter, Route, Redirect } from 'react-router-dom'
 import Table from './components/Table'
 import Pager from './components/Pager'
-import { connect } from 'react-redux'
-import { loadEmployees } from './store'
 
-class App extends Component {
-  constructor(props) {
-    super()
-  }
-  componentDidMount() {
-    console.log('App - on mount')
-    this.props.load()
-  }
-  render() {
-    console.log('App - in render')
-    return (
-      <main>
-        <h1>ACME Pager</h1>
-        <HashRouter>
-          <Route exact path="/:page">
-            <Table />
-            <Pager />
-          </Route>
-        </HashRouter>
-      </main>
-    )
-  }
+const App = () => {
+  return (
+    <main>
+      <h1>ACME Pager</h1>
+      <HashRouter>
+        <Route path="/:page" component={Table} />
+        <Route component={Pager} />
+      </HashRouter>
+    </main>
+  )
 }
 
-const mapStateToProps = null
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    load: () => {
-      console.log('loading data')
-      dispatch(loadEmployees(0))
-    },
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
