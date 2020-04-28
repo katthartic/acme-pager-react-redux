e90402563b502fe1d9d99dbd96f5dd7016b2d8c0
katthartic <katt.baum@gmail.com>
2020-04-28 13:24:48 -0400

    Adjust css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { HashRouter, Route, Redirect } from 'react-router-dom'
 import Table from './components/Table'
 import Pager from './components/Pager'
 
-const App = () => {
-  return (
-    <main>
-      <h1>ACME Pager</h1>
-      <HashRouter>
-        <Route path="/:page" component={Table} />
-        <Route component={Pager} />
-      </HashRouter>
-    </main>
-  )
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>ACME Pager</h1>
+        <HashRouter>
+          <Route path="/:page" component={Table} />
+          <Route path="/:page" component={Pager} />
+          <Redirect to="/0" />
+        </HashRouter>
+      </main>
+    )
+  }
 }
 
 export default App
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,36 +3,44 @@ import { connect } from 'react-redux'
 import { loadEmployees } from '../store'
 
 class Table extends Component {
-  constructor() {
-    super()
-    this.state = {
-      headers: [],
-    }
-  }
-
   componentDidMount() {
     const page = this.props.match.params.page
     this.props.load(page)
   }
 
   render() {
+    console.log(this.props.employees)
+    const employees = this.props.employees.rows || []
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Postion</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
+      <div id="table-container">
+        <table className="fixed_header">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Postion</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.email}</td>
+                <td>{employee.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
 
-const mapStateToProps = null
+const mapStateToProps = ({ employees }) => ({
+  employees,
+})
 
 const mapDispatchToProps = (dispatch) => {
   return {
